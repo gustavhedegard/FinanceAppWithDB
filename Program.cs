8a38e355237580b7d4dc487ae4f809422f509ca8
gustavhede <gustavhedegard@gmail.com>
2024-11-25 21:14:10 +0100

    set up connection to db

--- Program.cs
+++ Program.cs
@@ -1,9 +1,20 @@
 ﻿namespace FinanceApp;
+using Npgsql;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        string connectionString = "Host=localhost;Username=postgres;Password=password;Database=finance_app";
+
+        using var connection = new NpgsqlConnection(connectionString);
+
+        connection.Open();
+
+        var sql = "CREATE TABLE IF NOT EXISTS accounts (id UUID PRIMARY KEY, name TEXT NOT NULL)";
+
+        using var cmd = new NpgsqlCommand(sql, connection);
+
+        cmd.ExecuteNonQuery();
     }
 }
