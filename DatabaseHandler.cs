71e5f8b9ee6dfaa6c5ae3b152202d4b4a2a4a9b6
gustavhede <gustavhedegard@gmail.com>
2024-11-26 20:07:28 +0100

    add handler class for db

---
+++ DatabaseHandler.cs
@@ -0,0 +1,45 @@
+using Npgsql;
+public class DatabaseHandler {
+
+    private NpgsqlConnection npgsqlConnection;
+
+    public DatabaseHandler() {
+
+        string connectionString = "Host=localhost;Username=postgres;Password=password;Database=finance_app";
+
+        this.npgsqlConnection = new NpgsqlConnection(connectionString);
+
+        npgsqlConnection.Open();
+
+        ExecuteSqlCmd(CreateAccountTableQuery());
+        ExecuteSqlCmd(CreateTransactionTableQuery());
+    }
+
+    public void ExecuteSqlCmd(string query) {
+
+        using var executeSqlCmd = new NpgsqlCommand(query, npgsqlConnection); 
+        executeSqlCmd.ExecuteNonQuery();
+    }
+
+    public string CreateAccountTableQuery() {
+        
+        return @"CREATE TABLE IF NOT EXISTS accounts (
+            id UUID PRIMARY KEY,
+            user_name TEXT NOT NULL,
+            balance DECIMAL CHECK(balance <= 0)
+        )";
+    }
+
+    public string CreateTransactionTableQuery() {
+
+        return @"CREATE TABLE IF NOT EXISTS transactions (
+            id UUID PRIMARY KEY,
+            amount DECIMAL NOT NULL,
+            date TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+        )";
+    }
+
+    public void Close() {
+        npgsqlConnection.Close();
+    }
+}
--- Program.cs
+++ Program.cs
@@ -5,16 +5,18 @@ class Program
 {
     static void Main(string[] args)
     {
-        string connectionString = "Host=localhost;Username=postgres;Password=password;Database=finance_app";
+        var handler = new DatabaseHandler();
+        
+        // string connectionString = "Host=localhost;Username=postgres;Password=password;Database=finance_app";
 
-        using var connection = new NpgsqlConnection(connectionString);
+        // using var connection = new NpgsqlConnection(connectionString);
 
-        connection.Open();
+        // connection.Open();
 
-        var sql = "CREATE TABLE IF NOT EXISTS accounts (id UUID PRIMARY KEY, name TEXT NOT NULL)";
+        // var sql = "CREATE TABLE IF NOT EXISTS accounts (id UUID PRIMARY KEY, user_name TEXT NOT NULL, balance DECIMAL CHECK(balance <= 0)";
 
-        using var cmd = new NpgsqlCommand(sql, connection);
+        // using var cmd = new NpgsqlCommand(sql, connection);
 
-        cmd.ExecuteNonQuery();
+        // cmd.ExecuteNonQuery();
     }
 }
